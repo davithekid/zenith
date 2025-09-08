63a54822b603c27bd91f158dbcd340224a840901
davithekid <byjdrk.27@gmail.com>
2025-09-08 14:53:53 -0300

    feat(index): grid

--- src/components/blocks/Accordion.jsx
+++ src/components/blocks/Accordion.jsx
@@ -13,6 +13,8 @@ import {
         className="w-full"
         defaultValue="item-1"
       >
+        <div className="px-3">
+
         <AccordionItem value="item-1">
           <AccordionTrigger className={"font-bold"}>Templates informações</AccordionTrigger>
           <AccordionContent className="flex flex-col gap-4 text-balance">
@@ -33,10 +35,11 @@ import {
           <AccordionTrigger className={"font-bold"}>Qual a vantagem de ter um template?</AccordionTrigger>
           <AccordionContent className="flex flex-col gap-4 text-balance">
             <p>
-              Eficiencia e tudo mais.
+              Eficiencia e tudo.
             </p>
           </AccordionContent>
         </AccordionItem>
+        </div>
       </Accordion>
     )
   }
--- src/components/blocks/Separator.jsx
+++ src/components/blocks/Separator.jsx
@@ -4,14 +4,16 @@ import { CardDemo } from "./Card"
 export function SeparatorDemo() {
   return (
     <div>
-      <div className="space-y-1">
+      <div className="space-y-1 px-6">
         <h4 className="text-sm lg:text-2xl leading-none font-bold">Templates</h4>
         <p className="text-muted-foreground text-sm lg:text-sm">
           Adquire templates para ajudar o seu estudo
         </p>
       </div>
       <div className="flex justify-between py-6 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         <CardDemo />
+        </div>
 
       </div>
       <Separator className="my-4" />
