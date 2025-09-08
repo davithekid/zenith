d4c8d3bc00fe7206c8b4ad0b00a9a7882910f478
davithekid <byjdrk.27@gmail.com>
2025-09-08 15:07:32 -0300

    fix(layout): corrigido o erro do next relacionado a troca de temas

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="en" suppressHydrationWarning>
      <body>
           <ThemeProvider
             attribute="class"
